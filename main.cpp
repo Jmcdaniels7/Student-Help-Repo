e0e624c79fa4bc2c6b75ef50caec85564d70ac6d
Jmcdaniels7 <150567027+Jmcdaniels7@users.noreply.github.com>
2025-03-28 16:41:55 +0000

    new

--- main.cpp
+++ main.cpp
@@ -1,1 +1,32 @@
+#include <string>
+#include <iostream>
+#include <fstream>
 
+int main()
+{
+    std::ifstream inData;
+
+    inData.open("grade.txt");
+
+    int stuNum;
+    std::string stuName;
+    float stuPercent;
+    std::string stuGrade;
+
+
+    inData >> stuNum;
+
+    inData >> std::ws;
+
+    std::getline(inData, stuName, '0');
+
+    inData >> stuPercent;
+
+    inData >> stuGrade;
+
+    std::cout << "Student Number: " << stuNum << std::endl;
+    std::cout << "Name: " << stuName << std::endl;
+    std::cout << "Percentage: " << stuPercent << std::endl;
+    std::cout << "Grade: " << stuGrade << std::endl;
+
+}
